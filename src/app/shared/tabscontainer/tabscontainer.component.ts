8ea5a58ab387bbfde3aff6c54e524c19b27e97c5
“Kristiyan <kristiyan.merdzhov@stud.uni-due.de>
2022-11-09 19:22:54 +0100

    Rendering the tabs seperately

--- src/app/shared/tabscontainer/tabscontainer.component.ts
+++ src/app/shared/tabscontainer/tabscontainer.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { TabComponent } from './../tab/tab.component';
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-tabscontainer',
   templateUrl: './tabscontainer.component.html',
   styleUrls: ['./tabscontainer.component.css']
 })
-export class TabscontainerComponent implements OnInit {
+export class TabscontainerComponent implements AfterContentInit {
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList<TabComponent>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter(tab => tab.active)
+
+    if(!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => { tab.active = false; });
+
+    tab.active = true;
   }
 
 }
