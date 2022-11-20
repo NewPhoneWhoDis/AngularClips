b2a06eec5cb849f83a24ca7915d2014584946e24
“Kristiyan <kristiyan.merdzhov@stud.uni-due.de>
2022-11-20 19:38:16 +0100

    saving work

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class InputComponent implements OnInit {
   @Input() control: FormControl = new FormControl();
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
 
   constructor() { }
 
