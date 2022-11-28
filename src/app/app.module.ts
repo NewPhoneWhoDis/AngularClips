2213c6444cc17dab79741019dd42f8b3e57bd715
“Kristiyan <kristiyan.merdzhov@stud.uni-due.de>
2022-11-28 23:11:48 +0100

    Firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { UserModule } from './user/user.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,8 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA1eXFb11rFKb2HDOWXozp6rBj5K-2K09Y",
+    authDomain: "clipz-1a7ae.firebaseapp.com",
+    projectId: "clipz-1a7ae",
+    storageBucket: "clipz-1a7ae.appspot.com",
+    messagingSenderId: "1008873712590",
+    appId: "1:1008873712590:web:1d3de0473918e82bd8599f"
+  }
 };
