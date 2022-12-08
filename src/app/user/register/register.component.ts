d66984d4167f652ed9d1556674b3f1e0757e8f83
NewPhoneWhoDis <kristiyanmerdzhov@gmail.com>
2022-12-08 12:05:51 +0100

    Custom validators and async

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 
 @Component({
@@ -16,10 +18,10 @@ export class RegisterComponent  {
   alertColor: string = 'blue';
   inSubmission: boolean = false;
 
-  constructor(private auth: AuthService) {}
-
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
+//! check confirm_password for proper validators 
   name = new FormControl('', [Validators.required, Validators.minLength(3)])
-  email = new FormControl('', [Validators.required, Validators.email])
+  email = new FormControl('', [Validators.required, Validators.email], [this.emailTaken.validate])
   age = new FormControl<number | null>(null, [Validators.required, Validators.min(18), Validators.max(120)]) 
   password = new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)])
   confirm_password = new FormControl('', [Validators.required])
@@ -32,7 +34,7 @@ export class RegisterComponent  {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber
-  })
+  }, [RegisterValidators.match('password', 'confirm_password')])
 
   async register() {
     console.log("Submitted")
