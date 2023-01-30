cb706d0938c436e3684fc2ad10f71502900b5db2
Kristiyan Merdzhov <88944722+NewPhoneWhoDis@users.noreply.github.com>
2023-01-30 01:02:22 +0100

    Added implementation of uploadFile function

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -24,7 +26,7 @@ export class UploadComponent implements OnInit {
   })
 
 
-  constructor() { }
+  constructor(private storageService: AngularFireStorage) { }
 
   ngOnInit(): void {
   }
@@ -43,8 +45,10 @@ export class UploadComponent implements OnInit {
   }
 
   uploadFile() {
-    //TODO: Implement
-    console.log('File Uploaded')
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    this.storageService.upload(clipPath, this.file)
   }
 
 }
