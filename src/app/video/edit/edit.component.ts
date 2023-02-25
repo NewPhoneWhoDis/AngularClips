a39e8bfdc2249a408fec04449e40a2cc8e5721d4
Kristiyan Merdzhov <88944722+NewPhoneWhoDis@users.noreply.github.com>
2023-02-25 22:54:58 +0100

    Edit form controls

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,16 +1,37 @@
 import { ModalService } from './../../services/modal.service';
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
 import IClip from 'src/app/models/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy {
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null;
 
+  clipID = new FormControl('', { nonNullable: true });
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.maxLength(5)
+    ],
+    nonNullable: true
+  });
+
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID
+  })
+
   constructor(private modal: ModalService) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(!this.activeClip) return;
+    this.clipID.setValue(this.activeClip.docID as string);
+    this.title.setValue(this.activeClip.title as string);
+  }
 
   ngOnInit(): void {
     this.modal.register('editClip');
