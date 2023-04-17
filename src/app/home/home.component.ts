639743ec30149c85639d2aac0b72c4eb1444f15d
Kristiyan Merdzhov <88944722+NewPhoneWhoDis@users.noreply.github.com>
2023-04-17 18:38:41 +0200

    Clips list component with scroll event listener for infinite scrolling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   constructor() { }
-
+  
   ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight} = document.documentElement;
+    const { innerHeight } = window; 
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+    if(bottomOfWindow) {
+      console.log("test bottom of window");
+    }
   }
 
 }
