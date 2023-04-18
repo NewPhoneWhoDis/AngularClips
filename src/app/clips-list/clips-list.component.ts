d1fd545e9dd04f65e52eabeaa6c1439dd1e1ce60
Kristiyan Merdzhov <88944722+NewPhoneWhoDis@users.noreply.github.com>
2023-04-18 16:45:44 +0200

    Added the logic for rendering clips from the clipsList

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClipService } from '../services/clip.service';
 
 @Component({
   selector: 'app-clips-list',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClipsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public clipService: ClipService) {
+    this.clipService.getClips()
+  }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight} = document.documentElement;
+    const { innerHeight } = window; 
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+    if(bottomOfWindow) {
+      this.clipService.getClips()
+      console.log("Handle scroll activated");
+    }
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,31 +1,14 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
 
   constructor() { }
   
   ngOnInit(): void {
-    window.addEventListener('scroll', this.handleScroll);
   }
-  
-  ngOnDestroy(): void {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll = () => {
-    const { scrollTop, offsetHeight} = document.documentElement;
-    const { innerHeight } = window; 
-
-    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
-
-    if(bottomOfWindow) {
-      console.log("test bottom of window");
-    }
-  }
-
 }
